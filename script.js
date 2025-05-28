45d7da3bfeb0cc477128c5ab16a5ea9f715ca7cc
Manisha Priya <116943075+M1325-source@users.noreply.github.com>
2025-05-28 12:31:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Initialize the map
 const map = L.map('map').setView([20, 0], 2);
+let selectedYear = document.getElementById("year-select").value;
+let chart;
 
-// Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Fetch country geojson and add to map
 fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
   .then(res => res.json())
   .then(data => {
@@ -20,22 +19,83 @@ fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.g
       style: {
         color: '#333',
         weight: 1,
-        fillColor: '#ccc',
-        fillOpacity: 0.5
+        fillColor: '#80cbc4',
+        fillOpacity: 0.4
       }
     }).addTo(map);
   });
 
 function getCovidData(country) {
-  fetch(`https://disease.sh/v3/covid-19/countries/${country}`)
+  fetch(`https://disease.sh/v3/covid-19/historical/${country}?lastdays=all`)
     .then(res => res.json())
     .then(data => {
+      const timeline = data.timeline || data;
+
+      const casesByYear = {}, deathsByYear = {}, recoveredByYear = {};
+      for (let date in timeline.cases) {
+        const year = date.split('/')[2];
+        const y = "20" + year;
+        casesByYear[y] = (casesByYear[y] || 0) + timeline.cases[date];
+        deathsByYear[y] = (deathsByYear[y] || 0) + timeline.deaths[date];
+        recoveredByYear[y] = (recoveredByYear[y] || 0) + timeline.recovered[date];
+      }
+
+      const year = selectedYear;
       document.getElementById('country-name').innerText = data.country || country;
-      document.getElementById('cases').innerText = data.cases.toLocaleString();
-      document.getElementById('deaths').innerText = data.deaths.toLocaleString();
-      document.getElementById('recovered').innerText = data.recovered.toLocaleString();
+      document.getElementById('cases').innerText = (casesByYear[year] || 0).toLocaleString();
+      document.getElementById('deaths').innerText = (deathsByYear[year] || 0).toLocaleString();
+      document.getElementById('recovered').innerText = (recoveredByYear[year] || 0).toLocaleString();
+
+      renderChart(casesByYear, deathsByYear, recoveredByYear);
     })
     .catch(() => {
       alert("Data not available for " + country);
     });
+}
+
+document.getElementById("year-select").addEventListener("change", function () {
+  selectedYear = this.value;
+});
+
+function renderChart(cases, deaths, recovered) {
+  const labels = Object.keys(cases).sort();
+  const casesData = labels.map(y => cases[y] || 0);
+  const deathsData = labels.map(y => deaths[y] || 0);
+  const recoveredData = labels.map(y => recovered[y] || 0);
+
+  if (chart) chart.destroy();
+
+  chart = new Chart(document.getElementById("chart"), {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Cases',
+          data: casesData,
+          borderColor: 'blue',
+          fill: false
+        },
+        {
+          label: 'Deaths',
+          data: deathsData,
+          borderColor: 'red',
+          fill: false
+        },
+        {
+          label: 'Recovered',
+          data: recoveredData,
+          borderColor: 'green',
+          fill: false
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: { position: 'top' },
+        title: { display: true, text: 'COVID-19 Data by Year' }
+      }
+    }
+  });
 }
